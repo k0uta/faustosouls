5d6f0b241ed3dec52b41e1b6212eaff4649b321c
Murilo Costa <murilo.costa@tapps.com.br>
2016-09-17 11:08:43 -0300

    Boundaries added to ghost

--- faustosouls-project/Assets/EvilCacetadas/Scripts/GhostBehaviour.cs
+++ faustosouls-project/Assets/EvilCacetadas/Scripts/GhostBehaviour.cs
@@ -9,6 +9,12 @@ public class GhostBehaviour : MonoBehaviour {
 
 	private bool isWandering;
 
+	private Vector2 boundaries;
+
+	private Vector2 size;
+
+	public Transform background;
+
 	public float moveSpeed = 5.0f;
 
 	public float checkTimeThreshold = 2.0f;
@@ -20,12 +26,34 @@ public class GhostBehaviour : MonoBehaviour {
 		isWandering = true;
 		nextTarget = transform.position;
 		timeWithoutCheck = 0;
+
+		SpriteRenderer ghostSprite = transform.FindChild ("Sprite").GetComponent<SpriteRenderer> ();
+		SpriteRenderer backgroundSprite = background.FindChild("DefaultBackground").GetComponent<SpriteRenderer> ();
+		boundaries = new Vector2 ((float)backgroundSprite.bounds.size.x * 0.5f - (float)ghostSprite.bounds.size.x * 0.5f, (float)backgroundSprite.bounds.size.y * 0.5f - (float)ghostSprite.bounds.size.y * 0.5f);
+	}
+
+	float RandomSign() {
+		return (Random.value > 0.5f) ? -1.0f : 1.0f;
 	}
 
 	void AssignNewTarget() {
-		float xSign = (Random.value > 0.5f) ? -1.0f : 1.0f;
-		float ySign = (Random.value > 0.5f) ? -1.0f : 1.0f;
-		nextTarget = transform.position + new Vector3 (Random.Range (1, 2) * xSign, Random.Range (1, 2) * ySign);
+		nextTarget = transform.position + new Vector3 (Random.Range (1, 2) * RandomSign(), Random.Range (1, 2) * RandomSign());
+	}
+
+	void CheckBoundaries() {
+		Vector3 currentPosition = transform.position;
+		if(Mathf.Abs(currentPosition.x) > boundaries.x) {
+			float positionSign = Mathf.Abs (currentPosition.x) / currentPosition.x;
+			currentPosition = new Vector2 (boundaries.x * positionSign, currentPosition.y);
+		}
+		if(Mathf.Abs(currentPosition.y) > boundaries.y) {
+			float positionSign = Mathf.Abs (currentPosition.y) / currentPosition.y;
+			currentPosition = new Vector2 (currentPosition.x, boundaries.y * positionSign);
+		}
+		if (!currentPosition.Equals (transform.position)) {
+			transform.position = currentPosition;
+			AssignNewTarget ();
+		}
 	}
 
 	void Wander() {
@@ -33,6 +61,7 @@ public class GhostBehaviour : MonoBehaviour {
 			AssignNewTarget ();
 		} else {
 			transform.position = Vector3.MoveTowards (transform.position, nextTarget, moveSpeed * Time.deltaTime);
+			CheckBoundaries ();
 		}
 	}
 	
